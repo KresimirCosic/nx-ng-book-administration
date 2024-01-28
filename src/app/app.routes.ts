33f70031c2534b1be8500d538d97f2ea505b6dc0
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-01-28 09:35:15 +0100

    feat: removed header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BookComponent } from './pages/book/book.component';
+
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: ':id',
+    component: BookComponent,
+  },
+];
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { HeaderComponent } from '../header/header.component';
 import { PageComponent } from '../page/page.component';
 
 @Component({
   selector: 'nx-ng-book-administration-layout',
   standalone: true,
-  imports: [RouterModule, CommonModule, HeaderComponent, PageComponent],
+  imports: [RouterModule, CommonModule, PageComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
