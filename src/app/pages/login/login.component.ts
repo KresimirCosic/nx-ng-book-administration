ed7c61023fd3fd7710afbf092acc9e4fc4e6b640
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-01-28 17:36:14 +0100

    feat: login form added, api changes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,27 +1,47 @@
 import { CommonModule } from '@angular/common'
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
 import { Store } from '@ngrx/store'
 import { ButtonModule } from 'primeng/button'
+import { InputTextModule } from 'primeng/inputtext'
 
-import { login } from 'src/app/store/authentication/actions'
 import { AppState } from 'src/app/store/state'
 
 @Component({
   selector: 'nx-ng-book-administration-login',
   standalone: true,
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, InputTextModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  constructor(private readonly _store: Store<AppState>) {}
+export class LoginComponent implements OnInit {
+  loginFormGroup: FormGroup
 
-  login() {
-    this._store.dispatch(
-      login({
-        email: 'my@email.tld',
-        password: 'password',
-      })
-    )
+  constructor(
+    private readonly _store: Store<AppState>,
+    private _formBuilder: FormBuilder
+  ) {
+    this.loginFormGroup = this._formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  ngOnInit(): void {
+    // TODO
+  }
+
+  login(): void {
+    // this._store.dispatch(
+    //   login({
+    //     email: 'admin@domain.tld',
+    //     password: 'strong_password',
+    //   })
+    // )
   }
 }
