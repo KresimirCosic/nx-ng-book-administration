6bdd007e08137090984ec9636a80fd31d9175639
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-01-28 08:40:57 +0100

    feat: removed normalize.css, primeng default config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, ButtonModule],
   selector: 'nx-ng-book-administration-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nx-ng-book-administration';
+
+  constructor(private _primengConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this._primengConfig.ripple = true;
+  }
 }
