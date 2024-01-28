a583fe907dbbaefc4bc515d958c2e52ff76756c6
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-01-28 08:15:49 +0100

    feat: ngrx installed via nx, automatic setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideEffects(), provideStore(), provideRouter(appRoutes)],
 };
