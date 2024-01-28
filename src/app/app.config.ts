2fb83de4d5fb32751b1172d8f02182d5725da44a
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-01-28 09:24:55 +0100

    feat: page components added, formatting and cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
+
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
