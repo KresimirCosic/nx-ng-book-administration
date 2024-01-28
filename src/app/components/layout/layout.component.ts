dac491d140f83a6ed3967a66b63d98008a59397a
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-01-28 09:02:18 +0100

    feat: add basic layout components

---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nx-ng-book-administration-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {}
