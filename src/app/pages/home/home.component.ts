558b549c83c0298b51547db6e2938983eac07d1b
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-01-30 16:21:35 +0100

    feat: loaders and layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,40 @@
 import { CommonModule } from '@angular/common'
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { Store } from '@ngrx/store'
 import { ButtonModule } from 'primeng/button'
+import { ProgressSpinnerModule } from 'primeng/progressspinner'
+import { Observable } from 'rxjs'
 
-import { getBooks } from 'src/app/store/books/actions'
-import { AppState } from 'src/app/store/state'
 import { BookListComponent } from 'src/app/components/book-list/book-list.component'
+import { getBooks, unselectBooks } from 'src/app/store/books/actions'
+import { selectBooksAreLoading } from 'src/app/store/books/selectors'
+import { AppState } from 'src/app/store/state'
 
 @Component({
   selector: 'nx-ng-book-administration-home',
   standalone: true,
-  imports: [CommonModule, ButtonModule, BookListComponent],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    BookListComponent,
+    ProgressSpinnerModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
-  constructor(private readonly _store: Store<AppState>) {}
+export class HomeComponent implements OnInit, OnDestroy {
+  booksAreLoading$: Observable<boolean>
+
+  constructor(private readonly _store: Store<AppState>) {
+    this.booksAreLoading$ = this._store.select(selectBooksAreLoading)
+  }
 
   ngOnInit(): void {
     this.getBooks()
+  }
+
+  ngOnDestroy(): void {
+    this._store.dispatch(unselectBooks())
   }
 
   getBooks(): void {
